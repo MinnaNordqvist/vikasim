b4ddd2a46481d223afe3751acf4f8130cc136e8e
unknown <minna.nordqvist@edu.turkuamk.fi>
2024-11-28 16:50:34 +0200

    serveri metodit

--- main.js
+++ main.js
@@ -11,56 +11,78 @@ import { modifyVHW } from './hooks/VHW.js';
 //Tähän määritellään Stormwindin ja Clientin (=plotteri) portit ja IP:t
 const serverPort = config.ServerPort;
 const ServerAddress = config.ServerAddress;
+const ServerHost = config.ServerHost;
 const localP = config.StormWindLocalPort;
 const localAddr = config.StormWindLocalAddr;
 const clientPort = config.ClientLocalPort;
 const clientAddr = config.ClientLocalAddr;
-console.log("Serveri kuuntelee porttia: " + serverPort + " ja sen osoite on: " + ServerAddress);
+console.log("Serverin portti: " + serverPort + " ja sen osoite on: " + ServerAddress + " ja sen host on: " + ServerHost);
 console.log("Stormwind Portista: " + localP + " ja osoitteesta: " + localAddr);
 console.log("Plotteri portista: " + clientPort + " ja osoitteesta: " + clientAddr); 
 
 
-let server, istream = fs.createReadStream("./data/data.txt");
+//let server, istream = fs.createReadStream("./data/data.txt");
 //Luodaan serveri joka lukee tiedostosta "Stormwind dataa"
-server = net.createServer((socket) => {
-    socket.pipe(process.stdout);
-    istream.on("readable", function () {
-        let data;
-        while (data = this.read()) {
-            socket.write(data);
-        }
-    })
-    istream.on("end", function(){
-        socket.end();
-    }) 
-});
+//server = net.createServer((socket) => {
+//    socket.pipe(process.stdout);
+//    istream.on("readable", function () {
+//        let data;
+//        while (data = this.read()) {
+//            socket.write(data);
+//        }
+//    })
+//    istream.on("end", function(){
+//        socket.end();
+//    }) 
+//});
 
 //Luodaan serveri joka lukee dataa Stormwindista:
-// let server = net.createServer();
+let server = net.createServer();
 
 server.on('close',function(){
     console.log('Server closed !');
 });
 
 server.on('connection', (socket) =>{
+    socket.setEncoding('utf8');
     var address = server.address();
     var port = address.port;
     var family = address.family;
     var ipaddr = address.address;
-    console.log('Server is listening to port ' + port);
-    console.log('Server ip ' + ipaddr);
     console.log('Server is IP4/IP6 : ' + family);
-    var lport = socket.localPort;
-    var laddr = socket.localAddress;
-    console.log('Server LOCAL port ' + lport);
-    console.log('Server LOCAL ip ' + laddr);
-    socket.setEncoding('utf8');
+    console.log('Server is listening to port ' + port);
+    console.log("Server host: " + ServerHost);
+    //Näiden pitäisi olla samat:
+    console.log('Server ip ' + ipaddr + " and " + ServerAddress);
+    
+    var socketaddr = socket.address();
+    var ip = socketaddr.address;
+    var sport = socketaddr.port;
+    //Näiden pitäisi olla samat:
+    console.log('Socket port ' + sport + " and " + localP);
+    console.log('Socket ip ' + ip + " and " + localAddr);
+   
 
    //Tänne laitetaan mitä tehdään sitten kun ei lueta dataa tiedostosta
    socket.on('data', (data) =>{
-        //socket.write(data);
-        
-       // socket.on('end', socket.end);
+    let res="";
+    let message = "";
+    for (const chunk of data){
+        res += chunk;
+    }
+    message = res.split("\r\n").filter(a => !!a).map(b => locationLostRMC(b)).join("\r\n");
+    socket.write(message);
+    
+    //Jos Stormwind ei lähetä dataa, lukeminen laitetaan tauolle
+    if(!socket.write(message)){
+        socket.pause();
+    }
+
+    socket.on('drain', () =>{
+        socket.resume();
+    })   
+    
+    socket.on('end', socket.end);
     });
 });
 
@@ -68,8 +90,8 @@ server.on('error', (err) => {
     throw err;
 });
 
-//Dynaaminen portin valinta
-server.listen(() => {
+//Staattinen portin valinta. Kuunnellaan Stormwindin porttia
+server.listen({port: localP, host: ServerAddress},() => {
     var address = server.address();
     console.log("opened server on %j", address);
 });
@@ -79,16 +101,16 @@ server.getConnections((error,count) =>{
 });
 
 //Luodaan client joka lähettää muokatut arvot eteenpäin.
-var client  = net.connect({port: server.address().port, host:''}, function(){
+var client  = net.connect({port: localP, host: ServerHost}, function(){
     console.log('Client: connection established with server');
     var address = client.address();
+    var ip = address.address; 
+    var family = address.family; 
     var port = address.port;
-    var family = address.family;
-    var ipaddr = address.address;
-    console.log('Client port ' + port);
-    console.log('Client ip ' + ipaddr);
     console.log('Client is IP4/IP6 : ' + family);
-   
+    //Näiden pitäisi antaa samat arvot
+    console.log('Client port ' + clientPort + " and: " + port);
+    console.log('Client ip ' + clientAddr + " and: " + ip);
 });
 
 
@@ -97,22 +119,7 @@ client.on('data', (data) =>{
     //Jos pitää testata, saako client dataa serveriltä:
    // console.log(data);
     
-    let res="";
-    let message = "";
-    for (const chunk of data){
-        res += chunk;
-    }
-    message = res.split("\r\n").filter(a => !!a).map(b => locationLostRMC(b)).join("\r\n");
-    client.write(message);
-    
-    //Jos client ei lähetä dataa, lukeminen laitetaan tauolle
-    if(!client.write(message)){
-        client.pause();
-    }
-
-    client.on('drain', () =>{
-        client.resume();
-    })
+    client.write(data);
 
     client.on('end', client.end);
 });
