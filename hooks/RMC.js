c1faf366819080eb46c7314efea3024e5bf97e33
unknown <minna.nordqvist@edu.turkuamk.fi>
2024-12-10 18:01:27 +0200

    Moveship otettu huomioon minuutit

--- hooks/RMC.js
+++ hooks/RMC.js
@@ -4,8 +4,8 @@ import { verifyCS } from './checksum.js';
 //RMC = Recommended Minimum. 
 //$GPRMC,UTC_TIME,STATUS,LATITUDE,N/S,LONGITUDE,E/W,SPEED_KNOTS,DEGREES_TRUE,DATE,DEGREES_MAGNETIC,E/W,FAA*hh
 //Esim. $GPRMC,122332.58,A,5959.966064,N,02435.708935,E,40.3,337.1,281024,,,A*67
-//LATITUDE: ddmm.mmmmmm jossa dd = asteet ja mm.mmmmmm = minuutit
-//LONGITUDE: dddmm.mmmmmm jossa ddd = asteet ja mm.mmmmmm = minuutit
+//LATITUDE: ddmm.mmmmmm jossa dd = asteet ja mm.mmmmmm = minuutit [0000.000000, 9000.000000]
+//LONGITUDE: dddmm.mmmmmm jossa ddd = asteet ja mm.mmmmmm = minuutit [00000.000000, 18000.000000]
 //5959.966064,N = N 59° 59,966064’
 //02435.708935,E = E 024° 35,708935’
 const rmc = (input) => {
@@ -74,12 +74,18 @@ export const locationLostRMC = (input) => {
 
 let firstLat = 0;
 let firstLong = 0;
-
+let defLat = "N";
+let defLong = "E";
 const getLat = () => firstLat;
 const setLat = (val) => {firstLat = val;};
 const getLong = () => firstLong;
 const setLong = (val) => {firstLong = val;};
+const getDefLat = () => defLat;
+const setDefLat = (val) => {defLat = val;};
+const getDefLong = () => defLong;
+const setDefLong = (val) => {defLong = val;};
 
+//Oletetaan alussa että LATITUDE = N ja LONGITUDE = E
 const directions = [
     {dir: "N", lat:  0.011, long: 0},
     {dir: "S", lat:  -0.011, long: 0},
@@ -93,10 +99,16 @@ const directions = [
 
 //Manipuloidaan sijaintia haluttuun suuntaan, lasketaan uusi checksum ja palautetaan muutettu viesti. Ensimmäinen kutsu
 export const moveShip = (input, direction) => {
+    let latDeg;
+    let longDeg;
+    let latMin;
+    let longMin;
+    let lat;
+    let long;
     let modLat;
     let modLong;
     let going;   
-    
+
     if (input == null) {
         return console.log("Invalid input");
     }
@@ -114,11 +126,67 @@ export const moveShip = (input, direction) => {
     
     modLat = going.lat;
     modLong = going.long;
- 
+    
     let iterate = message.slice(1, -3).split(',');    
-    iterate[3] = (parseFloat(iterate[3]) + modLat).toFixed(6);
-    iterate[5] =  (parseFloat(iterate[5]) + modLong).toFixed(6);
-    if(iterate[5] < 10000){iterate[5] = 0 + iterate[5];}    
+    latDeg = iterate[3].slice(0, 2); // [0, 90]
+    latMin = iterate[3].slice(2, 11); // [0, 59.999]
+    lat = iterate[4]; // N/S
+    longDeg = iterate[5].slice(0,3); // [0, 180]
+    longMin = iterate[5].slice(3,12); // [0, 59.999]
+    long = iterate[6]; // E/W
+    //Tarkistetaan onko Lat N ja Long E, mikäli ei ole, vaihdetaan moderaattorien merkit +-
+    let chooseLat = (lat == "N") ? modLat : -(modLat);
+    let chooseLong = (long == "E") ? modLong : -(modLong);
+
+    // Lasketaan latituden muutos minuuteissa
+    latMin = (parseFloat(latMin) + chooseLat).toFixed(6);
+    if (latMin >= 60){
+        latDeg = parseInt(latDeg) + 1;
+        latMin = (parseFloat(latMin) - 60).toFixed(6);
+    }
+    if (latMin < 0){
+        latDeg = parseInt(latDeg) - 1;
+        latMin = (60 + parseFloat(latMin)).toFixed(6);
+    }
+  
+    //Mikäli Latitude menee negatiiviseksi laskutoimituksen jälkeen:
+    if (latDeg < 0 ) {
+        latDeg = Math.abs(latDeg);
+        let x = (lat == "N") ? "S" :
+                (lat == "S") ? "N" :
+        lat = x;     
+    }
+    //Palautetaan puuttuvat nollat
+    if (latMin < 10.00){latMin = 0 + latMin;}
+    if (latDeg < 10.00){latDeg = 0 + latDeg;}
+    iterate[3] = latDeg+latMin;
+
+    //Lasketaan longitude muutos minuuteissa
+    longMin = (parseFloat(longMin) + chooseLong).toFixed(6);
+    if (longMin >= 60){
+        longDeg = parseInt(longDeg) + 1;
+        longMin = (parseFloat(longMin) - 60).toFixed(6);
+    }
+    if (longMin < 0){
+        longDeg = parseInt(longDeg) - 1;
+        longMin = (60 + parseFloat(longMin)).toFixed(6);
+    }
+    
+    //Mikäli Longitude menee negatiiviseksi laskutoimituksen jälkeen:
+    if(longDeg < 0 ){
+        longDeg = Math.abs(longDeg);
+        let x = (long == "E") ? "W" :
+                (long == "W") ? "E" :
+        long = x;  
+    }
+
+    //Palautetaan nollat
+    if (longDeg < 10){longDeg = 0 + longDeg;}
+    if (longMin < 10){longMin = 0 + longMin;}
+    iterate[5] = longDeg+longMin;
+    
+    iterate[4] = lat;
+    iterate[6] = long;
     setLat(iterate[3]);
     setLong(iterate[5]);
 
@@ -132,6 +200,8 @@ export const moveShip = (input, direction) => {
 
 //Jatketaan sijainnin manipulointia edellisestä paikasta haluttuun suuntaan, lasketaan uusi checksum ja palautetaan muutettu viesti.
 export const moveShipAgain = (input, direction) => {
+    let lat;
+    let long;
     let modLat;
     let modLong;
     let going;   
@@ -156,30 +226,83 @@ export const moveShipAgain = (input, direction) => {
     modLong = going.long;
     startLat = getLat();
     startLong = getLong();
-    
+    lat = getDefLat();
+    long = getDefLong();
+
     let iterate = message.slice(1, -3).split(','); 
-    
+    //Tarkistetaan onko Lat N ja Long E, mikäli ei ole, vaihdetaan moderaattorien merkit +-
+    let chooseLat = (lat == "N") ? modLat : -(modLat);
+    let chooseLong = (long == "E") ? modLong : -(modLong);
+
     if (startLat !== 0 && startLong !== 0){
-        iterate[3] = (parseFloat(startLat) + modLat).toFixed(6);
-        iterate[5] = (parseFloat(startLong) + modLong).toFixed(6);
+        iterate[3] = (parseFloat(startLat) + chooseLat).toFixed(6);
+        //Mikäli Latitude menee negatiiviseksi laskutoimituksen jälkeen:
+        if (iterate[3] < 0 && lat === "N") {
+            iterate[3] = Math.abs(iterate[3]);
+            setDefLat("S");
+        }
+        if (iterate[3] < 0 && lat === "S"){
+            iterate[3] = Math.abs(iterate[3]);
+            setDefLat("N");
+        }
+        iterate[5] = (parseFloat(startLong) + chooseLong).toFixed(6);
+        //Mikäli Longitude menee negatiiviseksi laskutoimituksen jälkeen:
+         if(iterate[5] <= 0){
+            iterate[5] = Math.abs(iterate[5]).toFixed(6);
+            let x = (getDefLong() == "W")  ? "E" :
+                    (getDefLong() == "E") ? "W":
+            setDefLong(x);
+        }     
+        //Mikäli Longitude menee yli 180 asteen
+        if (iterate[5] > 18000) {
+            iterate[5] = (36000 - iterate[5]).toFixed(6);
+            let x = (getDefLong() == "W")  ? "E" :
+                    (getDefLong() == "E") ? "W":
+            setDefLong(x); 
+        }
+
+         //Palautetaan nolla, mikäli JavaScript poisti.
         if(iterate[5] < 10000){iterate[5] = 0 + iterate[5];}    
         setLat(iterate[3]);
         setLong(iterate[5]);
     } else {
-        iterate[3] = (parseFloat(iterate[3]) + modLat).toFixed(6);
-        iterate[5] = (parseFloat(iterate[5]) + modLong).toFixed(6);
+        iterate[3] = (parseFloat(iterate[3]) + chooseLat).toFixed(6);
+        if (iterate[3] < 0 && lat === "N") {
+            iterate[3] = Math.abs(iterate[3]);
+            setDefLat("S");
+        }
+        if (iterate[3] <= 0 && lat === "S"){
+            iterate[3] = Math.abs(iterate[3]);
+            setDefLat("N");
+        }
+        iterate[5] = (parseFloat(iterate[5]) + chooseLong).toFixed(6);
+         //Mikäli Longitude menee negatiiviseksi laskutoimituksen jälkeen:
+         if(iterate[5] <= 0){
+            iterate[5] = Math.abs(iterate[5]).toFixed(6);
+            let x = (getDefLong() == "W")  ? "E" :
+                    (getDefLong() == "E") ? "W":
+            setDefLong(x); 
+        }
+         //Mikäli Longitude menee yli 180 asteen
+         if (iterate[5] > 18000) {
+            iterate[5] = (36000 - iterate[5]).toFixed(6);
+            let x = (getDefLong() == "W")  ? "E" :
+                    (getDefLong() == "E") ? "W":
+            setDefLong(x); 
+        }
+         //Palautetaan nolla, mikäli JavaScript poisti.
         if(iterate[5] < 10000){iterate[5] = 0 + iterate[5];}    
         setLat(iterate[3]);
         setLong(iterate[5]);
     }
 
-    
+    iterate[4] = getDefLat();
+    iterate[6] = getDefLong();    
     let almost = iterate.toString();
     let cs = calculateCS(almost);
     let modified = `$${almost}*${cs}`
-        
-    return modified;
-}
 
+    return modified; 
+}
 
 export default rmc;
