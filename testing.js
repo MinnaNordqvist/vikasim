b8fbf488a382ecd9fcd7ce68da0a8077dc9156e0
unknown <minna.nordqvist@edu.turkuamk.fi>
2025-05-26 13:25:42 +0300

    kommentoitu koodia

--- testing.js
+++ testing.js
@@ -1,14 +1,18 @@
 import fs from 'node:fs';
 import readline from 'node:readline';
 
+//Tämä tiedosto on tarkoitettu funktioiden testaamiseen valmiilla datasetillä. 
+//Suorita ajamalla konsolissa käsky "node testing.js"
+
+//Hae testattavat funktiot kansiosta hooks
 import { moveShip } from './hooks/RMC.js';
 import { moveShipAgain } from './hooks/RMC.js';
+
 var data = [];
 let message = "";
 let iterate;
 
-
-
+//Tiedosto data.txt luetaan tiedostosta
 async function processLineByLine() {
     const filestream = fs.createReadStream('./data/data.txt');
     const rl = readline.createInterface({
@@ -27,6 +31,8 @@ async function processLineByLine() {
 
 await processLineByLine();     
 console.log("Hello world!");
+
+//Testaa haluamasi funktiot täällä ja tulosta manipuloitu viesti konsoliin.
 for(var i = 0; i < data.length - 1; i++){
     let j = i + 1;
     if(data[i].match("RMC")){
